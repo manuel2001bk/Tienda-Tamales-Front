258cf6a651da5f1ca9d6c50db2393c874a3ba4d4
manuel2001bk <61120758+manuel2001bk@users.noreply.github.com>
2022-06-01 10:55:51 -0500

    Ventana modal al formulario modificar tamal

--- app/Components/tamales/tamal.js
+++ app/Components/tamales/tamal.js
@@ -14,10 +14,13 @@ class tamal extends React.Component {
             listTamales : [],
             oculto : true,
             status: false,
-            show : false
+            show : false,
+            showUpdate : false
         }
         this.showModal = this.showModal.bind(this);
         this.hideModal = this.hideModal.bind(this);
+        this.showModalUpdate = this.showModalUpdate.bind(this);
+        this.hideModalUpdate = this.hideModalUpdate.bind(this);
         this.listTamales = []
 
         APIInvoker.invokeGET('/tamales/getAllTamales', data => {
@@ -127,6 +130,13 @@ class tamal extends React.Component {
     hideModal = () => {
         this.setState({ show: false });
     };
+    showModalUpdate = () => {
+        this.setState({ showUpdate: true });
+    };
+
+    hideModalUpdate = () => {
+        this.setState({ showUpdate: false });
+    };
 
     render() {
         return(
@@ -135,9 +145,8 @@ class tamal extends React.Component {
                     <div id="addCliente"
                          ref={self => this.addCliente = self}>
                         <div className="card container">
-                            <h3>Agregar cliente</h3>
+                            <h4>Añadir Tamales</h4>
                                 <div className="card-body pt-5">
-                                    <h4>Añadir Tamales</h4>
                                     <form className="form-horizontal">
                                         <div className="form-floating">
                                             <input className="form-control"
@@ -189,6 +198,66 @@ class tamal extends React.Component {
                                         <div ref={self => this.messageError = self}></div>
                                     </form>
                                 </div>
+                        </div>
+                    </div>
+                </Modal>
+                <Modal show={this.state.showUpdate} handleClose={this.hideModalUpdate}>
+                    <div id="editCliente"
+                         ref={self => this.addCliente = self}>
+                        <div className="card overflow-hidden container">
+                            <div className="card-body pt-5">
+                                <h4>Editar Tamal</h4>
+                                <form className="form-horizontal">
+                                    <div className="form-floating">
+                                        <input className="form-control"
+                                               type="text"
+                                               name="nombre"
+                                               id="nombre"
+                                               placeholder="Manuel"
+                                               value={this.state.nombre}
+                                               onChange={this.changeField.bind(this)}/>
+                                        <label htmlFor="nombre">Nombre</label>
+                                    </div>
+                                    <label ref={self => this.nombre = self}
+                                           className="form-text text-danger"/>
+                                    <div className="form-floating">
+                                        <input className="form-control"
+                                               type="text"
+                                               name="direccion"
+                                               id="direccion"
+                                               placeholder="Calle"
+                                               value={this.state.direccion}
+                                               onChange={this.changeField.bind(this)}/>
+                                        <label htmlFor="direccion">Dirección</label>
+                                    </div>
+                                    <label ref={self => this.direccion = self}
+                                           className="form-text text-danger"/>
+                                    <div className="form-floating">
+                                        <input className="form-control"
+                                               type="text"
+                                               name="numeroTelefono"
+                                               id="numeroTelefono"
+                                               placeholder="968-855-96-74"
+                                               value={this.state.numeroTelefono}
+                                               onChange={this.changeField.bind(this)}/>
+                                        <label htmlFor="numeroTelefono">Numero de Telefono</label>
+                                    </div>
+                                    <label ref={self => this.numeroTelefono = self}
+                                           className="form-text text-danger"/>
+                                    <br/>
+                                    <div className="d-grid gap-2">
+                                        <button className="btn btn-outline-success"
+                                                type="button"
+                                                >Modificar
+                                        </button>
+                                        <button className="btn btn-outline-danger"
+                                                type="button"
+                                                onClick={this.hideModalUpdate}>Cancelar
+                                        </button>
+                                    </div>
+                                    <div ref={self => this.messageError = self}/>
+                                </form>
+                            </div>
                         </div>
                     </div>
                 </Modal>
@@ -253,6 +322,19 @@ class tamal extends React.Component {
                                                                             d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
                                                                         <path fill="evenodd"
                                                                               d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+                                                                    </svg>
+                                                                </button>
+                                                            </td>
+                                                            <td>
+                                                                <button className="btn btn-light"
+                                                                        value={item.idTamal}
+                                                                        onClick={this.showModalUpdate}>
+                                                                    <svg xmlns="http://www.w3.org/2000/svg"
+                                                                         width="16" height="16" fill="currentColor"
+                                                                         className="bi bi-pencil-fill"
+                                                                         viewBox="0 0 16 16">
+                                                                        <path
+                                                                            d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/>
                                                                     </svg>
                                                                 </button>
                                                             </td>
